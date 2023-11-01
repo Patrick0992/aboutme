380d5f09b1c1f81b00ce38de6d56f1cb8f8ef80a
Patrick0992 <o1103019@gm.pu.edu.tw>
2023-11-01 15:04:11 +0800

    make it better

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from datetime import datetime
+from datetime import datetime, timezone, timedelta
 
 app = Flask(__name__)
 
@@ -17,7 +17,9 @@ def db():
 
 @app.route("/tcyang", methods=["GET", "POST"])
 def tcyang():
-    now = str(datetime.now())
+    #now = str(datetime.now())
+    tz = timezone(timedelta(hours=+8))
+    now = datetime.now(tz)
     user = request.values.get("nick")
     return render_template("tcyang.html", datetime=now, name=user)
 
